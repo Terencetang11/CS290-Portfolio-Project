77a15240a5aa68ebdd2ad106725b4834beb39acb
Terencetang11 <59594491+Terencetang11@users.noreply.github.com>
2020-08-03 17:41:27 -0700

    updated handlebar templates

--- node.js
+++ node.js
@@ -50,18 +50,19 @@ app.get('/random-number', function(req,res){
     res.send("This is the random number page.  The random number is: " + num)
 });
 
+// handles 404 status code issues
 app.use(function(req,res){
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
-});
-
-app.use(function(err, req, res, next){
+    res.render('404');
+  });
+  
+  // handles 500 status code issues
+  app.use(function(err, req, res, next){
     console.error(err.stack);
     res.type('plain/text');
     res.status(500);
-    res.send('500 - Server Error');
-});
+    res.render('500');
+  }); 
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
